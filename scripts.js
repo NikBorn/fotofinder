39dcab06ed511933c7121bb0db1fbbec132338d2
Nik Bornemeier <NikolausRB@gmail.com>
2017-03-30 10:19:50 -0600

    fix some of the CSS issues

--- scripts.js
+++ scripts.js
@@ -1,6 +1,6 @@
-
 $('#save-btn').on('click', function() {
 addCard();
+resetForm();
 });
 
 function addCard() {
@@ -9,38 +9,48 @@ function addCard() {
   var $photoImageBad = $('#file').val();
   var $photoImage = $photoImageBad.replace(/^.*\\/, "");
   var $cardCatalog = $('.card-container');
-  console.log($photoImage)
-  $cardCatalog.prepend(`<article class="photo-card"><h3>${$photoTitle}</h3><img class="card-image" src="photos/${$photoImage}"><p class="card-cap">${$photoCaption}</p>   <div class= "button-icons"><div class="delete-btn card-btns"></div><div class="favorite-btn card-btns"></div></div></article>`);
+  $cardCatalog.prepend(`<article class="photo-card"><h3>${$photoTitle}</h3><img class="card-image" width="250px" height="150px" src="photos/${$photoImage}"><p class="card-cap">${$photoCaption}</p>   <div class= "button-icons"><div class="delete-btn card-btns"></div><div class="favorite-btn card-btns"></div></div></article>`);
 };
 
+function resetForm() {
+$('#title-input').val("");
+$('#caption-input').val("");
+$('#file').val("");
+$('#save-btn').prop('disabled', true)
+}
+
 $('.card-container').on('click', '.delete-btn', function(){
-  $(this).parent().parent().remove()
-  console.log($(this).parent().parent());
+  $(this).parent().parent().remove();
 })
 
-// function checkComplete() {
-//   var $photoTitle = $('#title-input').val();
-//   var $photoCaption = $('#caption-input').val();
-//   var $photoImage = $('#file').val();
-//   var $saveBtn = $('#save-btn');
-//   if ($photoTitle === "" || $photoCaption === ""  || $photoImage === "" ) {
-//     $('#save-btn').prop('disabled', 'disabled')
-//   } else if ($photoTitle !== "" || $photoCaption !== ""  || $photoImage !== "" ) {
-//     $('#save-btn').prop('disabled', 'enable'))
-//   }
-// }
+function checkComplete() {
+  if ($('#title-input').val().length > 0 && $('#caption-input').val().length > 0  && $('#file').val().length > 0 ) {
+    $('#save-btn').prop('disabled', false);
+  } else {
+    $('#save-btn').prop('disabled', true);
+  }
+}
 
-$('.card-container').on('click', '.favorite-btn', function(){
+function toggleFavorite() {
   var $pictureCard = $('.photo-card');
   var $favButton = $('.favorite-btn');
-  $(this).parent().parent().toggleClass('favorite-background')
-  $(this).parent().siblings().toggleClass('favorite-background')
+  $(this).parent().parent().toggleClass('favorite-background');
+  $(this).parent().siblings().toggleClass('favorite-background');
+  $(this).toggleClass('favorite-color');
+}
 
-  $(this).toggleClass('favorite-color')
-  })
+$('.card-container').on('click', '.favorite-btn', function(){
+  toggleFavorite()
+});
 
+$('#title-input').blur(function(){
+  checkComplete();
+});
 
+$('#caption-input').blur(function(){
+  checkComplete();
+});
 
-// $('body').keyup(function(){
-//   checkComplete();
-// })
+$('#file').change(function(){
+  checkComplete();
+});
